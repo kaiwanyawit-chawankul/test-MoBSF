738216dac0ec4417c7e0bd383f4598017fb851d9
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2023-03-13 11:30:31 +0700

    First commit

---
+++ android/app/src/main/java/org/jobjingjo/mobsf/MainActivity.java
@@ -0,0 +1,5 @@
+package org.jobjingjo.mobsf;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.jobjingjo.mobsf',
+  appName: 'test-mobsf',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
